5817f9d520e9eed34e9b329ec515273725590ff9
Aminafaraoui337 <farraouiamina@gmail.com>
2023-12-11 17:32:47 -0800

    dynamic libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P https://github.com/Aminafaraoui337/alx-low_level_programming/raw/a9cfe0f1ca741b4adae20807ff576214d44b8cc8/0x18-dynamic_libraries/libgiga.so
+wget -P /tmp https://github.com/Aminafaraoui337/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
 export LD_PRELOAD=/tmp/libgiga.so
